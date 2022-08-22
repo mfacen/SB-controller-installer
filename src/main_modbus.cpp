387fc9935c29015151980f702194735a7cab877d
michel facen <mfacen@hotmail.com>
2022-08-22 18:21:47 -0500

    added VFD types

--- src/main_modbus.cpp
+++ src/main_modbus.cpp
@@ -1,5 +1,5 @@
 
-#include "../common-libs/header.h"
+#include "../../SharpBox/include/header.h"
 
 #define DEVICE_NAME "relayBoardModbus"   //  Aqui es importante define el nombre para los updates es el mismo para los dispositivos del mismo tipo
 #define SOFT_VERSION "1.25"        //   Changed file system to LittleFS   CHECAR LINEA 311
@@ -19,7 +19,7 @@ DirCapture dirCapture("dCapt", "/capturas");
 //Set modLedSet ("Led","idLed",&ledModbus);
 ModbusRelay RelayBlowerSup(1,1);
 ModbusRelay relayResiduosSup(1,2);
-ModbusRelay RelayClarSup(1,3);
+ModbusRelay bombaClarSup(1,3);
 ModbusRelay evClarSup(1,4);
 ModbusRelay SkmSup(1,5);
 ModbusRelay RelayFeederAirSup(1,6);
@@ -28,16 +28,16 @@ ModbusRelay relayLucesSup(1,8);
 
 ModbusRelay RelayBlowerInf(1,9);
 ModbusRelay relayResiduosInf(1,10);
-ModbusRelay RelayClarInf(1,11);
+ModbusRelay bombaClarInf(1,11);
 ModbusRelay evClarInf(1,12);
 ModbusRelay SkmInf(1,13);
 ModbusRelay RelayFeederAirInf(1,14);
 ModbusRelay RelayFeederFeedInf(1,15);
 ModbusRelay relayLucesInf(1,16);
 //BinaryOutput spdSupCtrl( 22, 24, 25) ;
-ModbusVFD spdSupCtrl( 2) ;
-ModbusVFD spdInfCtrl( 3) ;
-Set vfdSup ("Venturi SUp","vfdSup",&spdSupCtrl);
+ModbusVFD spdSupCtrl( 2 , VFD_Types::SOYAN_SVD) ;
+ModbusVFD spdInfCtrl( 3 , VFD_Types::MOLLOM_B20) ;
+Set vfdSup ("Venturi sup","vfdSup",&spdSupCtrl);
 Set vfdInf ("Venturi inf","vfdInf",&spdInfCtrl);
 //Modbus_device pressureSensorSup (2,17); //17=A0 en esp8266 Aqui tengo que usar los numeros xQ estoy en ambiente esp32
 //  y se va a referir a numeros del esp8266 a travez del modbus
@@ -59,13 +59,13 @@ GenericTimer residuosInf("Residuos Inf", "ri", &relayResiduosInf);
 GenericTimer lucesSup("Luces Sup", "ls", &relayLucesSup);
 GenericTimer lucesInf("Luces Inf", "li", &relayLucesInf);
 
-Clarificador clarificadorSup("Clarificador Sup", "cs", &RelayClarSup, &evClarSup, &logger);
-Clarificador clarificadorInf("Clarificador Inf", "ci", &RelayClarInf, &evClarInf, &logger);
+Clarificador clarificadorSup("Clarificador Sup", "cs", &bombaClarSup, &evClarSup, &logger);
+Clarificador clarificadorInf("Clarificador Inf", "ci", &bombaClarInf, &evClarInf, &logger);
 
 Speed_Control spdSup("Speed Ctrl Sup", "spd_sup",&spdSupCtrl,&pressureSensorSup, &logger);
 Speed_Control spdInf("Speed Ctrl Inf", "spd_inf", &spdInfCtrl,&pressureSensorInf, &logger);
 
-#include "../common-libs/footer.h"
+#include "../../SharpBox/include/footer.h"
 
 // Button btnWifi("switchToStation","WiFi");
 // Graphic graphic("tempGraph");
@@ -82,12 +82,13 @@ void setup()
     SerialInterface.begin(9600, SERIAL_8N1, 16, 17);
     nodeRelays.begin(SerialInterface);
     savedVariable::init();
+    
     spdInf.init();
     spdSup.init();
     logger.addInput(&clarificadorInf);
     page.addElement(&lblTime);
-    page.addElement(&vfdSup);
-    page.addElement(&vfdInf);
+    //page.addElement(&vfdSup);
+    //page.addElement(&vfdInf);
     page.addString("<p>"
                    "<a href=/settings>Preferencias</a>"
                    "</p>"
@@ -134,12 +135,13 @@ void setup()
 ////                                                               ////////
 ///////////////////////////////////////////////////////////////////////////
 unsigned long lastCheck;
+int x=0;
 void loop()
 {
     generalLoop();
-    if (millis() - lastCheck > 2000)
+
+    if (millis() - lastCheck > 1000)
     {
-        //serialCom.requestData("spd_supmbar");
-        lastCheck = millis();
+        
     }
 }
