c354df8c2e19c1bb2e090af81d53578308582388
¨mfacen¨ <mfacen@hotmail.com>
2023-04-30 01:26:19 -0600

    2.22 Extra Output For Feeders

--- src/main_new.cpp
+++ src/main_new.cpp
@@ -2,7 +2,7 @@
 #include "../common-libs/header.h"
 
 #define DEVICE_NAME "relayBoardModbus"   //  Aqui es importante define el nombre para los updates es el mismo para los dispositivos del mismo tipo
-#define SOFT_VERSION "2.187"        //   Changed file system to LittleFS   CHECAR LINEA 311
+#define SOFT_VERSION "2.22"        //   Changed file system to LittleFS   CHECAR LINEA 311
 String mdnsName = DEVICE_NAME;     // "basementController.local" no hace falta saber el IP
 const char *OTAName = DEVICE_NAME; // A name and a password for the OTA service
 
@@ -120,6 +120,7 @@ void setup()
     //Serial.println(spdInf.pressure->id);
     page.addElement(&lblTime);
     //page.addElement(&vfdSup);
+ 
     //page.addElement(&vfdInf);
     page.addString("<p>"
                    "<a href=/settings>Preferencias</a>"
