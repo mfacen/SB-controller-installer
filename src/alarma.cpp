a0a42621b7b114b28b0d9cbdc3ab91d3dfd31583
michelfacen <mfacen@hotmail.com>
2024-11-28 12:24:17 -0600

    2 files

---
+++ src/alarma.cpp
@@ -0,0 +1,111 @@
+
+#include "../common-libs/header.h"
+
+#define DEVICE_NAME "relayBoardModbus"   //  Aqui es importante define el nombre para los updates es el mismo para los dispositivos del mismo tipo
+#define SOFT_VERSION "2.4"        //   Changed file system to LittleFS   CHECAR LINEA 311
+String mdnsName = DEVICE_NAME;     // "basementController.local" no hace falta saber el IP
+const char *OTAName = DEVICE_NAME; // A name and a password for the OTA service
+
+Page page("Shrimpbox Main Controller", "User interface");
+Updater updater(DEVICE_NAME, SOFT_VERSION);
+
+//Alarm alarma;
+TelegramAlarm alarma;
+TimeLabel lblTime("lblTime", "");
+LabelFreeHeap lblFreeHeap("lblHeap", "this");
+Logger logger("Logger", "/dataLog.csv", 60, &wifiClient);
+Label lblVersion("lblVersion", "Device: " + String(DEVICE_NAME) +
+                   " - Ver: " + String(SOFT_VERSION)+
+                   " - ID = "+mdnsName);
+DirCapture dirCapture("dCapt", "/capturas");
+
+
+Digital_Alarm alarm32("a32",32, &alarma);
+Digital_Alarm alarm33("a33",33, &alarma);
+Digital_Alarm alarm25("a25",25, &alarma);
+Digital_Alarm alarm26("a26",26, &alarma);
+
+#include "../common-libs/footer.h"
+
+// Button btnWifi("switchToStation","WiFi");
+// Graphic graphic("tempGraph");
+///////////////////////////////////////////////////////////////////////////
+////                                                               ////////
+///////              SETUP                                          ////////
+////                                                               ////////
+///////////////////////////////////////////////////////////////////////////
+
+void setup()
+{
+    startUpWifi();
+    //Serial2.begin(115200, SERIAL_8N1, 16, 17);
+    SerialInterface.begin(9600, SERIAL_8N1, 16, 17);
+    //nodeRelays.begin(SerialInterface);
+    // modbus.onData([](uint8_t serverAddress, esp32Modbus::FunctionCode fc, uint16_t address, uint8_t* data, size_t length) {
+    // Serial.printf("id 0x%02x fc 0x%02x len %u: 0x", serverAddress, fc, length);
+    // for (size_t i = 0; i < length; ++i) {
+    //   Serial.printf("%02x", data[i]);
+    // }
+    // std::reverse(data, data + 4);  // fix endianness
+    // Serial.printf("\nval: %.2f", *reinterpret_cast<float*>(data));
+    // Serial.print("\n\n");
+  //});
+  
+    //Serial.println(spdInf.pressure->id);
+   
+    page.addElement(&lblTime);
+    //page.addElement(&vfdSup);
+ 
+    //page.addElement(&vfdInf);
+    page.addString("<p>"
+                   "<a href=/settings>Preferencias</a>"
+                   "</p>"
+                    "<h3>Tanque Inferior</h3><div>");
+    //page.addElement(&alarm32);
+    page.addString("<div class='card'>");
+   // page.addElement(&flowMeterPanel);
+   // page.addElement(&flowMeterPanel1);
+    page.addElement(&alarm32);
+    page.addElement(&alarm33);
+    page.addElement(&alarm25);
+    page.addElement(&alarm26);
+    page.addString("</div>");
+    page.addElement(&logger);
+    //page.addString("</div>");
+     page.addElement(&dirCapture);
+    page.addString("<br>");
+    page.addElement(&lblVersion);
+    page.addString("<br>");
+    page.addElement(&lblFreeHeap);
+    // page.addElement(&btnWifi);
+    //page.setDebug(true); //
+
+    Serial.println("page.getHTML()");
+
+    page.getHtml(); // Generate the index.html File
+    Serial.println("Generated index.html");
+    updater.checkUpdate();
+    MapFile::saveLog("Start up unixTime= "+String(timeUNIX));
+}
+
+///////////////////////////////////////////////////////////////////////////
+////                                                               ////////
+///////              LOOP                                          ////////
+////                                                               ////////
+///////////////////////////////////////////////////////////////////////////
+unsigned long lastCheck;
+unsigned long lastAlarmSup;
+unsigned long lastAlarmInf;
+int alarmInterval = 600000;
+int x=0;
+void loop(){
+
+    generalLoop();
+
+
+
+    if (millis() - lastCheck > 10000)
+    {
+        lastCheck = millis();
+      }
+}
